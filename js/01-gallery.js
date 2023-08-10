9878c3ff3823d15ba7c2a24f4b37936d44a80525
TonyShimm <101990825+TonyShimm@users.noreply.github.com>
2023-08-10 08:28:35 -0400

    Final commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,85 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+// create modal
+// function onImgClick(e) {
+//   e.preventDefault();
+//   // if (e.target.nodeName !== "IMG") return;
+
+//   const isItemImage = e.target.classList.contains('gallery__image');
+//   if (!isItemImage) return;
+
+//   const currentImgUrl = e.target.dataset.source;
+
+//   const instance = basicLightbox.create(
+//     `
+// 		<img src="${currentImgUrl}" width="1280" height="auto"/>
+//         `,
+//     {
+//       onShow: (instance) => {
+//         window.addEventListener('keydown', onEscKeyPress);
+//       },
+//       onClose: (instance) => {
+//         window.removeEventListener('keydown', onEscKeyPress);
+//       },
+//     }
+//   );
+//   instance.show();
+
+//   function onEscKeyPress(e) {
+//     const ESC_KEY_CODE = 'Escape';
+//     const isEscKey = e.code === ESC_KEY_CODE;
+//     if (!isEscKey) return;
+//     instance.close();
+//   }
+// }
+
+// ------- 2nd variant --------------------------------------------------------------------------------
+
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
+}
